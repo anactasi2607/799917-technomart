562219b5b1ffbe7ae8962a94f43017d83d38c103
anactasi2607 <anactasi2607@yandex.ru>
2018-12-22 20:58:16 +0300

    Переделала скрипт, внесены правки согласно предыдущим замечаниям

--- js/popup.js
+++ js/popup.js
@@ -6,61 +6,44 @@ var userName = popup.querySelector(".user-name");
 var form = popup.querySelector("form");
 var userMail = popup.querySelector(".user-mail");
 var comment = popup.querySelector("[name=comment]");
-var isStorageSupport = true;
-var storage = ".userName";
 var mapLink = document.querySelector(".map-link");
 var mapPopup = document.querySelector(".modal-map");
 var mapClose = mapPopup.querySelector(".modal-close");
 
-try {
-  storage = localStorage.getItem("userName");
-} catch (err) {
-  isStorageSupport = false;
-}
-
-try {
-  storage = localStorage.getItem("userMail");
-} catch (err) {
-  isStorageSupport = false;
-}
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-
-  if (storage) {
-    userName.value = storage;
-    userMail.focus();
-  } else {
-    userName.focus();
-  }
-  
-  if (storage) {
-    userMail.value = storage;
-    comment.focus();
-  } else {
-    userMail.focus();
-  }
+  userName.focus();
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
+  userName.classList.remove("hint");
+  userMail.classList.remove("hint");
 });
 
 form.addEventListener("submit", function (evt) {
-   if (!userName.value || !userMail.value || !comment.value) {
+   if (!userName.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
-} else {
-    if (isStorageSupport) {
-    localStorage.setItem("userName", userName.value);
-    localStorage.setItem("userMail", userMail.value);
-}
-}
+    userName.classList.remove("hint");
+    userName.classList.add("hint");
+} 
+    if (!userMail.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+    userMail.classList.remove("hint");
+    userMail.classList.add("hint");
+
+} 
+
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -81,4 +64,14 @@ mapLink.addEventListener("click", function (evt) {
 mapClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+      mapPopup.classList.remove("modal-error");
+    }
+  }
 });
