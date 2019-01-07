f66053c0df67049eefac2aa0af54e572c795f8b7
anactasi2607 <anactasi2607@yandex.ru>
2019-01-08 00:06:42 +0300

    Начало скрипта для кнопок "Купить" и "В закладки"

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,7 @@
 
 var link = document.querySelector(".write-us-button");
 var popup = document.querySelector(".write-us");
-var close = popup.querySelector(".modal-close");
+var close = document.querySelector(".modal-close");
 var userName = popup.querySelector(".user-name");
 var form = popup.querySelector("form");
 var userMail = popup.querySelector(".user-mail");
@@ -9,7 +9,13 @@ var comment = popup.querySelector("[name=comment]");
 var mapLink = document.querySelector(".map-link");
 var mapPopup = document.querySelector(".modal-map");
 var mapClose = mapPopup.querySelector(".modal-close");
-
+var buyLink = document.querySelector(".buy");
+var popupBuy = document.querySelector(".cart");
+var goOn = document.querySelector(".white-button");
+var closeBuy = document.querySelector(".modal-close");
+var basket = document.querySelector(".basket");
+var marker = document.querySelector(".marker");
+var markerList = document.querySelector(".marker-list");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -74,4 +80,34 @@ window.addEventListener("keydown", function (evt) {
       mapPopup.classList.remove("modal-error");
     }
   }
+});
+
+buyLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupBuy.classList.add("modal-show");
+  basket.classList.add("basket-not-empty");
+});
+
+closeBuy.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupBuy.classList.remove("modal-show");
+});
+
+goOn.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupBuy.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popupBuy.classList.contains("modal-show")) {
+      popupBuy.classList.remove("modal-show");
+    }
+  }
+});
+
+marker.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  markerList.classList.add("marker-not-empty");
 });
